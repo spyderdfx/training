26909c5c2f4c51ade16d554f0b8b5652821ea537
Mikhail Nelaev <mikhail.nelaev@gmail.com>
2015-04-14 09:55:28 +0500

    style(all): tabs replaced with double spaces
    task4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	#set_primary_key "id"
-	#self.primary_key = [:id]
-	belongs_to :rubric
+  #set_primary_key "id"
+  #self.primary_key = [:id]
+  belongs_to :rubric
 end
